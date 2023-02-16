55ea828f85d160fc9aec7d114d0f1a2abadb9e92
Replit user <>
2023-02-16 02:01:12 +0000

    Edit more sidebar grid

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const nameElements = document.getElementsByClassName("userName");
 
-for (let i = 0; i < nameElements.length; i++){
-    nameElements[i].style.innerText = "Peter";
+window.onload = () => {
+  for (let i = 0; i < nameElements.length; i++) {
+    nameElements[i].style.innerText = "Peter Bachowski";
+  }
 }
+
