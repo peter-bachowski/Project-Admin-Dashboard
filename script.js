97681ca5bd2f7e38279c9b8fcdfe5cf44ac01609
Replit user <>
2023-02-17 01:54:46 +0000

    Add to script.js for adding links in the card elements

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const nameElements = document.getElementsByClassName("userName");
+const nameElements = document.getElementsByClassName("userName"), cardLinkElements = document.getElementsByClassName("cardContent");
+
+const img1 = document.createElement("img");
+const img2 = document.createElement("img");
+const img3 = document.createElement("img");
 
 window.onload = () => {
+
+  createCardLinks();
+  for (let i = 0; i < cardLinkElements.length; i++){
+    cardLinkElements[i].appendChild(img1);
+    cardLinkElements[i].appendChild(img2);
+    cardLinkElements[i].appendChild(img3);
+  }
+
   for (let i = 0; i < nameElements.length; i++) {
     nameElements[i].style.innerText = "Peter Bachowski";
   }
+}
+
+function createCardLinks() {
+  const cardLinkDiv = document.createElement("div");
+  cardLinkDiv.style.display = "flex";
+
+
+  img1.src = "./icons/star-plus-outline.png";
+  img2.src = "./icons/eye-plus-outline.png";
+  img3.src = "./icons/share-variant-outline.png";
+
 }
 
