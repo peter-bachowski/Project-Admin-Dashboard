12c1542f86f4a2dcbd404e8bef1ada1658ab032f
Peter <peter@Peters-MacBook-Pro.local>
2023-02-16 23:30:18 -0800

    Edit script.js for card layout

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const nameElements = document.getElementsByClassName("userName"), cardLinkElements = document.getElementsByClassName("cardContent");
-
-const img1 = document.createElement("img");
-const img2 = document.createElement("img");
-const img3 = document.createElement("img");
+const nameElements = document.getElementsByClassName("userName"),
+cardLinkElements = document.getElementsByClassName("cardContent");
 
 window.onload = () => {
 
   createCardLinks();
   for (let i = 0; i < cardLinkElements.length; i++){
-    cardLinkElements[i].appendChild(img1);
-    cardLinkElements[i].appendChild(img2);
-    cardLinkElements[i].appendChild(img3);
+    cardLinkElements[i].appendChild(document.createElement("img"));
+    cardLinkElements[i].appendChild(document.createElement("img"));
+    cardLinkElements[i].appendChild(document.createElement("img"));
   }
 
   for (let i = 0; i < nameElements.length; i++) {
@@ -18,14 +15,20 @@ window.onload = () => {
   }
 }
 
-function createCardLinks() {
-  const cardLinkDiv = document.createElement("div");
-  cardLinkDiv.style.display = "flex";
+// function createCardLinks() {
+//   const cardLinkDiv = document.createElement("div");
+//   cardLinkDiv.style.display = "flex";
 
+//   img1.src = "./icons/star-plus-outline.png";
+//   img1.width = 50;
+//   img1.height = 50;
 
-  img1.src = "./icons/star-plus-outline.png";
-  img2.src = "./icons/eye-plus-outline.png";
-  img3.src = "./icons/share-variant-outline.png";
+//   img2.src = "./icons/eye-plus-outline.png";
+//   img2.width = 50;
+//   img2.height = 50;
 
-}
+//   img3.src = "./icons/share-variant-outline.png";
+//   img3.width = 50;
+//   img3.height = 50;
+// }
 
