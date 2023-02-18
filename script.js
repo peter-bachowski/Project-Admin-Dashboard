827a0898529338c5cc38a27aecbce5c5705ff420
Replit user <>
2023-02-18 01:45:18 +0000

    Edit javascript for creating repeating image elements in the card container

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 const nameElements = document.getElementsByClassName("userName"),
-cardLinkElements = document.getElementsByClassName("cardContent");
+  cardElements = document.getElementsByClassName("cardContent");
 
 window.onload = () => {
-
-  createCardLinks();
-  for (let i = 0; i < cardLinkElements.length; i++){
-    cardLinkElements[i].appendChild(document.createElement("img"));
-    cardLinkElements[i].appendChild(document.createElement("img"));
-    cardLinkElements[i].appendChild(document.createElement("img"));
+  let cardLinkDivArray[];
+  let cardLinkArray[];
+  for (let i = 0; i < cardElements.length; i++) {
+    cardLinkDivArray[i] = document.createElement("div");
+    cardLinkArray[i] = document.createElement("img");
   }
 
+
+
+
   for (let i = 0; i < nameElements.length; i++) {
-    nameElements[i].style.innerText = "Peter Bachowski";
+    nameElements[i].innerHTML = "Peter Bachowski";
   }
 }
 
-// function createCardLinks() {
-//   const cardLinkDiv = document.createElement("div");
-//   cardLinkDiv.style.display = "flex";
 
-//   img1.src = "./icons/star-plus-outline.png";
-//   img1.width = 50;
-//   img1.height = 50;
-
-//   img2.src = "./icons/eye-plus-outline.png";
-//   img2.width = 50;
-//   img2.height = 50;
-
-//   img3.src = "./icons/share-variant-outline.png";
-//   img3.width = 50;
-//   img3.height = 50;
-// }
 
