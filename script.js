a635ac6ba50840b43a49184397552e33b653894c
Peter <peter@Peters-MacBook-Pro.local>
2023-02-18 12:17:22 -0800

    Add javascript for generating the icons to the cards

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const nameElements = document.getElementsByClassName("userName"),
   cardElements = document.getElementsByClassName("cardContent");
 
-window.onload = () => {
-  let cardLinkDivArray[];
-  let cardLinkArray[];
-  for (let i = 0; i < cardElements.length; i++) {
-    cardLinkDivArray[i] = document.createElement("div");
-    cardLinkArray[i] = document.createElement("img");
+window.onload = () => {//when the page loads
+
+  const iconDiv = document.createElement("div"); //creates a div container element for the card icons
+  iconDiv.classList.add("iconContainer");//adds class to div
+
+  const starPlusIcon = document.createElement("img"); //creates the icon elements
+  const shareIcon = document.createElement("img");
+  const eyePlusIcon = document.createElement("img");
+
+  starPlusIcon.src = "./icons/star-plus-outline.png"; //adds the source location for the images of the icons
+  shareIcon.src = "./icons/eye-plus-outline.png";
+  eyePlusIcon.src = "./icons/share-variant-outline.png";
+
+  starPlusIcon.classList.add("cardIcons");
+  eyePlusIcon.classList.add("cardIcons");
+  shareIcon.classList.add("cardIcons");
+
+  iconDiv.appendChild(starPlusIcon); //adds the icons to the div container
+  iconDiv.appendChild(eyePlusIcon);
+  iconDiv.appendChild(shareIcon);
+
+  for (let i = 0; i < cardElements.length; i++) { //for each card element, clone the icon div container and add it to the card content
+    cardElements[i].appendChild(iconDiv.cloneNode(true));
   }
 
-
-
-
-  for (let i = 0; i < nameElements.length; i++) {
+  for (let i = 0; i < nameElements.length; i++) {//fix this
     nameElements[i].innerHTML = "Peter Bachowski";
   }
 }
